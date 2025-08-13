244300f5cd59475dbde3a16cb8a9981be62f69ef
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2025-08-13 17:49:58 +0300

    bugfix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import ItemCard from './components/ItemCard';
 import items from './data/items';
 import './App.css';
